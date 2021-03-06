187c17701e9b2b412c01dc2ceb1d41c992b68b38
Benjamin Hackl <devel@benjamin-hackl.at>
2021-03-06 11:05:22 +0100

    factor out construction of reply into separate function, should fix weird typing behavior

--- DiscordManimator.py
+++ DiscordManimator.py
@@ -52,21 +52,7 @@ def construct(self):
 @commands.guild_only()
 async def manimate(ctx, *, arg):
 
-    async def react_and_wait(reply):
-        await reply.add_reaction("\U0001F5D1") # Trashcan emoji
-
-        def check(reaction, user):
-            return str(reaction.emoji) == '\U0001F5D1' and user == ctx.author
-
-        try:
-            reaction, user = await bot.wait_for('reaction_add', check=check, timeout=60.0)
-        except asyncio.TimeoutError:
-            await reply.remove_reaction("\U0001F5D1", bot.user)
-        else:
-            await reply.delete()
-
-    reply = None
-    async with ctx.typing():
+    def construct_reply(arg):
         if arg.startswith('```'): # empty header
             arg = '\n' + arg
         header, *body = arg.split('\n')
@@ -78,24 +64,22 @@ async def manimate(ctx, *, arg):
             "-t", "--transparent"
         ]
         if not all([flag in allowed_flags for flag in cli_flags]):
-            reply = await ctx.reply("You cannot pass CLI flags other than "
+            reply_args = {"content": "You cannot pass CLI flags other than "
                             "`-i` (`--save_as_gif`), `-s` (`--save_last_frame`), "
-                            "`-t` (`--transparent`).")
-            await react_and_wait(reply)
-            return
+                            "`-t` (`--transparent`)."}
+            return reply_args
         else:
             cli_flags = ' '.join(cli_flags)
 
         body = '\n'.join(body).strip()
 
         if body.count('```') != 2:
-            reply = await ctx.reply(
-                'Your message is not properly formatted. '
+            reply_args = {
+                "content": 'Your message is not properly formatted. '
                 'Your code has to be written in a code block, like so:\n'
                 '\\`\\`\\`py\nyour code here\n\\`\\`\\`'
-            )
-            await react_and_wait(reply)
-            return
+            }
+            return reply_args
 
         script=re.search(
             pattern = r"```(?:py)?(?:thon)?(.*)```",
@@ -118,10 +102,11 @@ async def manimate(ctx, *, arg):
             with open(scriptfile, 'w', encoding='utf-8') as f:
                 f.write('\n'.join(script))
             try: # now it's getting serious: get docker involved
+                reply_args = None
                 container_stderr = dockerclient.containers.run(
                     image="manimcommunity/manim:stable",
                     volumes={tmpdirname: {'bind': '/manim/', 'mode': 'rw'}},
-                    command=f"timeout 120 manim /manim/script.py -qm --disable_caching -o scriptoutput {cli_flags}",
+                    command=f"timeout 120 manim /manim/script.py -qm --disable_caching --progress_bar False -o scriptoutput {cli_flags}",
                     user=os.getuid(),
                     stderr=True,
                     stdout=False,
@@ -129,37 +114,58 @@ async def manimate(ctx, *, arg):
                 )
                 if container_stderr:
                     if len(container_stderr.decode('utf-8')) <= 1200:
-                        reply = await ctx.reply(
-                            "Something went wrong, here is "
+                        reply_args = {
+                            "content": "Something went wrong, here is "
                             "what Manim reports:\n"
                             f"```\n{container_stderr.decode('utf-8')}\n```"
-                        )
+                        }
                     else:
-                        reply = await ctx.reply(
-                            "Something went wrong, here is "
+                        reply_args = {
+                            "content": "Something went wrong, here is "
                             "what Manim reports:\n",
-                            file=discord.File(
+                            "file": discord.File(
                                 fp=io.BytesIO(container_stderr),
                                 filename="Error.log",
-                            ),
-                        )
-                    await react_and_wait(reply)
-                    return
+                            )
+                        }
+
+                    return reply_args
 
             except Exception as e:
-                reply = await ctx.reply(f"Something went wrong: ```{e}```")
-                await react_and_wait(reply)
+                reply_args = {"content": f"Something went wrong: ```{e}```"}
                 raise e
+            finally:
+                if reply_args:
+                    return reply_args
+
             try:
                 [outfilepath] = Path(tmpdirname).rglob('scriptoutput.*')
             except Exception as e:
-                reply = await ctx.reply("Something went wrong: no (unique) output file was produced. :cry:")
-                await react_and_wait(reply)
+                reply_args = {"content": "Something went wrong: no (unique) output file was produced. :cry:"}
                 raise e
             else:
-                reply = await ctx.reply("Here you go:", file=discord.File(outfilepath))
-                await react_and_wait(reply)
+                reply_args = {"content": "Here you go:", "file": discord.File(outfilepath)}
+            finally:
+                return reply_args
 
+    async def react_and_wait(reply):
+        await reply.add_reaction("\U0001F5D1") # Trashcan emoji
+
+        def check(reaction, user):
+            return str(reaction.emoji) == '\U0001F5D1' and user == ctx.author
+
+        try:
+            reaction, user = await bot.wait_for('reaction_add', check=check, timeout=60.0)
+        except asyncio.TimeoutError:
+            await reply.remove_reaction("\U0001F5D1", bot.user)
+        else:
+            await reply.delete()
+
+    async with ctx.typing():
+        reply_args = construct_reply(arg)
+        reply = await ctx.reply(**reply_args)
+
+    await react_and_wait(reply)
     return
 
 
