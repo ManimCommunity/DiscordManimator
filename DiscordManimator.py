2aa16ebb842bb6a39c24676a97f223d85f4b27d8
Benjamin Hackl <devel@benjamin-hackl.at>
2021-03-06 12:21:17 +0100

    prototype for mdoc command

--- DiscordManimator.py
+++ DiscordManimator.py
@@ -169,4 +169,42 @@ async def manimate(ctx, *, arg):
     return
 
 
+@bot.command()
+@commands.guild_only()
+async def mdoc(ctx, *args):
+    if len(args) == 0:
+        await ctx.reply(
+            "Pass some manim function or class and I will find the "
+            "corresponding documentation for you. Example: `!mdoc Square`"
+        )
+        return
+
+    arg = args[0]
+    if not arg.isidentifier():
+        await ctx.reply(f"`{arg}` is not a valid identifier, no class or function can be named like that.")
+        return
+
+    try:
+        container_output = dockerclient.containers.run(
+            image="manimcommunity/manim:stable",
+            command=f"""timeout 10 python -c "import manim; assert '{arg}' in dir(manim); print(manim.__version__ + '|' + manim.{arg}.__module__ + '.{arg}')" """,
+            user=os.getuid(),
+            stderr=False,
+            stdout=True,
+            detach=False,
+            remove=True
+        )
+    except docker.errors.ContainerError as e:
+        if 'AssertionError' in e.args[0]:
+            await ctx.reply(f"I could not find `{arg}` in our documentation, sorry.")
+            return
+        await ctx.reply(f"Something went wrong: ```{e.args[0]}```")
+        return
+    
+    version, fqname = container_output.decode("utf-8").strip().split("|")
+    url = f"https://docs.manim.community/en/v{version}/reference/{fqname}.html"
+    await ctx.reply(f"Here you go: {url}")
+    return
+    
+
 bot.run(TOKEN, bot=True, reconnect=True)
