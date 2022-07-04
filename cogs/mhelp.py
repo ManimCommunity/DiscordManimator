9bf2b753e402a316e1e5803e5ad77978a131b371
Eshaan Naga Venkata <73739511+nveshaan@users.noreply.github.com>
2022-07-05 03:58:37 +0530

    Updated supported tags (#34)
    * Updated supported tags
    
    * Update cogs/mhelp.py
    
    Co-authored-by: Kiran-Raj-Dev <90276965+Kiran-Raj-Dev@users.noreply.github.com>
    
    * formatted
    
    Co-authored-by: Kiran-Raj-Dev <90276965+Kiran-Raj-Dev@users.noreply.github.com>

--- cogs/mhelp.py
+++ cogs/mhelp.py
@@ -15,7 +15,7 @@ Code **must** be properly formatted and indented. Note that you can't animate th
 
 Supported tags:
 ```
-    -t, --transparent, -i, --save_as_gif, -s, --save_last_frame
+    -i (--format=gif), -s (--save_last_frame), -t (--transparent), --renderer=opengl, --use_projection_fill_shaders, --use_projection_stroke_shaders
 ```
 Example:
 ```
